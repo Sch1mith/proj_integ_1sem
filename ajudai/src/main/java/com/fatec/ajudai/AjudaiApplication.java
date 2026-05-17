5a773293f37f9905b405eeba2fcc347c8661dffb
Schimith <schimith.guilherme@gmail.com>
2026-05-17 19:11:06 -0300

    server spring boot configurado

---
+++ ajudai/src/main/java/com/fatec/ajudai/AjudaiApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.ajudai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjudaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjudaiApplication.class, args);
+	}
+
+}
